b18baa588e7e10f855b1456b93cd61c0b63c35ab
Darius <darius@darius.(none)>
2013-08-26 17:40:02 +0300

    basic api doc annotation handler

--- src/Evispa/ResourceApiDocBundle/AnnotationHandler/ResourceAnnotationHandler.php
+++ src/Evispa/ResourceApiDocBundle/AnnotationHandler/ResourceAnnotationHandler.php
@@ -8,7 +8,30 @@
 namespace Evispa\ResourceApiDocBundle\AnnotationHandler;
 
 
-class ResourceAnnotationHandler
-{
+use Evispa\Component\MultipartResource\Annotations\Resource;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Nelmio\ApiDocBundle\Extractor\HandlerInterface;
+use Nelmio\ApiDocBundle\Extractor\Nelmio;
+use Nelmio\ApiDocBundle\Extractor\ReflectionMethod;
+use Nelmio\ApiDocBundle\Extractor\Symfony;
+use Symfony\Component\Routing\Route;
 
+class ResourceAnnotationHandler implements HandlerInterface
+{
+    /**
+     * Parse route parameters in order to populate ApiDoc.
+     *
+     * @param \Nelmio\ApiDocBundle\Annotation\ApiDoc $annotation
+     * @param array                                  $annotations
+     * @param \Symfony\Component\Routing\Route       $route
+     * @param \ReflectionMethod                      $method
+     */
+    public function handle(ApiDoc $apiAnnotation, array $annotations, Route $route, \ReflectionMethod $method)
+    {
+        foreach ($annotations as $annotation) {
+            if ($annotation instanceof Resource) {
+                $apiAnnotation->setSection($annotation->getName());
+            }
+        }
+    }
 }
