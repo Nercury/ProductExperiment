99cb54a759f3b083f56cc6ea8c5d820f61545c12
Nerijus Arlauskas <nercury@gmail.com>
2013-08-29 15:25:30 +0300

    readme

--- src/Evispa/Api/Product/Tests/CodeTest.php
+++ src/Evispa/Api/Product/Tests/CodeTest.php
@@ -27,23 +27,27 @@
 
 namespace Evispa\Api\Product\Tests;
 
+use Doctrine\Common\Annotations\AnnotationReader;
+use Evispa\Api\Product\Model\Code\CodeV1;
+use Evispa\Api\Product\Model\Code\ProductCodeV1;
+use Evispa\ObjectMigration\VersionConverter;
+use Evispa\ObjectMigration\VersionReader;
+
 class CodeTest extends \PHPUnit_Framework_TestCase
 {
     public function testVersionAnnotations() {
-        $reader = new \Doctrine\Common\Annotations\AnnotationReader();
-        $versionReader = new \Evispa\ObjectMigration\VersionReader($reader);
-        $converter = new \Evispa\ObjectMigration\VersionConverter($versionReader);
+        $converter = new VersionConverter(new VersionReader(new AnnotationReader()));
 
         // create object
 
-        $code = new \Evispa\Api\Product\Model\Code\ProductCodeV1();
+        $code = new ProductCodeV1();
         $code->code = "Hello";
 
         // migrate to another version
 
         $simpleCode = $converter->migrate($code, 'vnd.evispa.code.v1');
 
-        $this->assertTrue($simpleCode instanceof \Evispa\Api\Product\Model\Code\CodeV1);
+        $this->assertTrue($simpleCode instanceof CodeV1);
         $this->assertEquals("Hello", $simpleCode->code);
     }
 }
