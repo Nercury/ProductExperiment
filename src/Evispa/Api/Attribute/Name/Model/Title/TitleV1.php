3ddfeb529ba637b20273f12a4d691e77297e52dc
Tadcka <tadcka@forfree.(none)>
2013-09-13 15:13:37 +0300

    Add attribute api

---
+++ src/Evispa/Api/Attribute/Name/Model/Title/TitleV1.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Company: Evispa
+ * User: Tadas
+ * Date: 9/13/13
+ * Time: 2:49 PM
+ */
+
+
+namespace Evispa\Api\Attribute\Name\Model\Title;
+
+
+class Title
+{
+
+}
---
+++ src/Evispa/Api/Attribute/Value/Model/SimpleAttributeValueV1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Company: Evispa
+ * User: Tadas 
+ * Date: 9/13/13
+ * Time: 2:45 PM
+ */
+
+
+class SimpleAttributeValueV1 {
+
+}
